9e2a50dca5e251cb86ca24300bfb32106ce94db3
Raphael Pontes <raphael.pontes@condoconta.com.br>
2023-01-31 16:16:58 -0300

    upd

--- lib/app/external/datasources/user_current_imp_datasource.dart
+++ lib/app/external/datasources/user_current_imp_datasource.dart
@@ -1,8 +1,11 @@
 // ignore_for_file: depend_on_referenced_packages
 
+import 'dart:convert';
+
 import 'package:flutter_modular/flutter_modular.dart';
 import '../../data/datasources/user_current_datasource.dart';
 import 'package:flut_micro_commons_client_https/flut_micro_commons_client_https.dart';
+import 'package:flut_micro_commons_shared/flut_micro_commons_shared.dart';
 
 final $UserCurrentImpDatasource = Bind.lazySingleton(
   (i) => UserCurrentImpDatasource(i()),
@@ -16,9 +19,16 @@ class UserCurrentImpDatasource implements UserCurrentDatasource {
   @override
   Future<Map<String, dynamic>> call() async {
     try {
-      var res = await _clientHttps.get('/api/user');
+      var storage = await LocalStorage.get('user');
+      var user = storage != null ? json.decode(storage) : null;
 
-      return res.data;
+      if (user != null) {
+        return {"success": true, "data": user};
+      } else {
+        var res = await _clientHttps.get('/api/user');
+        await LocalStorage.set('user', json.encode(res.data["data"]));
+        return res.data;
+      }
     } catch (e) {
       rethrow;
     }
